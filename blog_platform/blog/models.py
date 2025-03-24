f1531131f859afa7bd0378521aae90429cb17d54
qwissx <borutenkobogdan@gmail.com>
2025-03-24 21:24:56 +0300

    admin panel

--- blog_platform/blog/models.py
+++ blog_platform/blog/models.py
@@ -3,6 +3,14 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return (
+            super(PublishedManager,self)
+            .get_queryset()
+            .filter(status='published'))
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ("draft", "Draft"),
