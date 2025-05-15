940e4afe095830205d7b895b4041a2b3483d3bfd
qwissx <borutenkobogdan@gmail.com>
2025-05-15 11:41:17 +0300

    add comments system

--- blog_platform/blog/views.py
+++ blog_platform/blog/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 
-from blog.models import Post
-from blog.forms import EmailPostForm
+from blog.models import Post, Comment
+from blog.forms import EmailPostForm, CommentForm
 from blog_platform.settings import EMAIL_HOST_USER
 
 
@@ -64,8 +64,27 @@ def post_detail(request, year, month, day, slug):
         publish__day=day,
         slug=slug,
     )
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+
+    else:
+        comment_form = CommentForm()
+
     return render(
         request,
         'blog/post/detail.html',
-        {'post': post},
+        {
+            'post': post,
+            'comments': comments,
+            'new_comment': new_comment,
+            'comment_form': comment_form,
+        },
     )
